d1bf49626b357bb0f239d4ddb969cda32ddbea0f
vignkkum <84365904+vignkkum@users.noreply.github.com>
2021-06-01 10:38:57 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,23 @@
+package programs;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hello="Hello World";
+		String[] Car= {"BMW", "AUDI", "FORD"};
+		System.out.println("The Length of the String is: " + hello.length());
+		System.out.println(Car[1]);
+		System.out.println(Car.length);
+		System.out.println(hello.charAt(1));
+		int num1=15;
+		System.out.println("First:"+ num1 );
+		num1 +=20; //num1=num1+20;
+		System.out.println("After:"+ num1 );
+		float floatnum=15;
+		char chardata='A';
+		boolean status=true;
+System.out.println(status);
+	}
+
+}
